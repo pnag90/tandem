6319d27831fb6ad5f4d7f7756e0b9ebe471b33b2
Pedro Goncalves <pedro.goncalves@first-global.com>
2015-12-08 01:00:53 +0000

    Controllers config... ok

--- src/main/java/tandem/security/LoginController.java
+++ src/main/java/tandem/security/LoginController.java
@@ -1,5 +1,8 @@
 package tandem.security;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import tandem.model.Users;
+import tandem.utils.HibernateUtil;
 
 import java.util.Date;
+import java.util.List;
 import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletResponse;
@@ -19,26 +24,79 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/security/login")
 public class LoginController {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-    
-    @RequestMapping (value={"/teste"},  method = { RequestMethod.POST, RequestMethod.GET } )
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sf){
+		this.sessionFactory = sf;
+	}
+
+	@RequestMapping (value={"/teste"},  method = { RequestMethod.POST, RequestMethod.GET } )
 	public @ResponseBody Date getData() {
 		Date d = new Date();
 		System.out.println("Estou a usar o cenas...");
 		return d;
 	}
 
-    @RequestMapping(value = {"/auth"}, method = RequestMethod.POST)
-    @ResponseBody
-    public Users auth(@RequestBody TreeMap<String, Object> credentials, HttpServletResponse response) throws Exception {
-        // simulate delay to test UI loader/spinner
-        Thread.sleep(2000);
-        logger.info("inside login - user: " + credentials);
-        
-        if (!credentials.get("username").toString().isEmpty() && credentials.get("password").toString().isEmpty()) {
-            return new Users("abc", "1234567890");
-        }
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return new Users();
-    }
+	@RequestMapping(value = {"/auth"}, method = RequestMethod.POST)
+	public @ResponseBody TreeMap<String, Object> auth(@RequestBody TreeMap<String, Object> credentials) {
+		TreeMap<String, Object> res = new TreeMap<String, Object>();
+		try {		
+			if(credentials != null){
+				logger.info("inside login - user: " + credentials);
+
+				if (credentials.get("username")!=null && credentials.get("password")!=null) {
+					Users user;
+					Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+					if (!credentials.get("username").toString().isEmpty() && !credentials.get("password").toString().isEmpty()) {
+						user = new Users();
+						user.setUsername(credentials.get("username").toString());
+						user.setPassword(credentials.get("password").toString());
+						res.put("result", user);
+						res.put("isError", false);
+						res.put("error", "");
+					}else{
+						res.put("result", null);
+						res.put("isError", true);
+						res.put("error", "Erro java...");
+						System.out.println("/security/login/auth : faltam parametros");
+					}
+				}else{
+					res.put("result", null);
+					res.put("isError", true);
+					res.put("error", "Faltam Parametros");
+					System.out.println("/security/login/auth : parametros null");
+				}
+				//response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			}
+		} catch (Exception e) {
+			logger.error(e.toString());
+			res.put("isError", true);
+			res.put("error", e.toString());
+			res.put("result", null); 
+		}
+		return res;
+	}
+
+	public boolean checkLogin(String userName, String userPassword){
+		System.out.println("In Check login");
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		boolean userFound = false;
+
+		//Query using Hibernate Query Language
+		String SQL_QUERY =" from tandem.Users as o where o.username=? and o.password=?";
+		Query query = session.createQuery(SQL_QUERY);
+		query.setParameter(0,userName);
+		query.setParameter(1,userPassword);
+		List list = query.list();
+
+		if ((list != null) && (list.size() > 0)) {
+			userFound= true;
+		}
+
+		session.close();
+		return userFound;              
+	}
 }
--- src/main/java/tandem/utils/CORSFilter.java
+++ src/main/java/tandem/utils/CORSFilter.java
@@ -13,32 +13,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-* Implements a filter for REST connections
-* Avoids Access-Control-Allow-Origin errors on the client
-*/
+ * Implements a filter for REST connections
+ * Avoids Access-Control-Allow-Origin errors on the client
+ */
 
 //@Component
 public class CORSFilter implements Filter {
-  private static final String ORIGIN_HEADER = "origin";
-  
+	private static final String ORIGIN_HEADER = "origin";
+
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {}
-	
+
 	@Override
-  public void doFilter(ServletRequest req, ServletResponse resp,FilterChain chain) throws IOException, ServletException {       
-      HttpServletRequest request=(HttpServletRequest) req;
-      HttpServletResponse response=(HttpServletResponse) resp;
-      String origin = request.getHeader(ORIGIN_HEADER);
-      
+	public void doFilter(ServletRequest req, ServletResponse resp,FilterChain chain) throws IOException, ServletException {       
+		HttpServletRequest request=(HttpServletRequest) req;
+		HttpServletResponse response=(HttpServletResponse) resp;
+		String origin = request.getHeader(ORIGIN_HEADER);
+
 		response.setHeader("Access-Control-Allow-Origin",origin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-	    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-	    response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		//response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		//response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		//response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 
-      chain.doFilter(req, resp);
-  }	
+		chain.doFilter(req, resp);
+	}	
 
 	@Override
 	public void destroy() {}
